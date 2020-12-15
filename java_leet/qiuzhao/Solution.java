44aaa32d7541936150ff870e85db828c4981db88
Noeverer <robotliu0327@gmail.com>
2020-12-15 10:05:46 +0800

    Readme.md

---
+++ java_leet/qiuzhao/Solution.java
@@ -0,0 +1,12 @@
+package qiuzhao;
+
+class Solution {
+    public int translateNum(int num) {
+
+        return  1;
+    }
+
+    public static int main(String[] args) {
+        return 1;
+    }
+}
